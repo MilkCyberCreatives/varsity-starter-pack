203ebbe91253c8e7cf0124ba37fb4f39989b6f5c
YOUR NAME <YOUR EMAIL>
2026-01-27 13:11:15 +0200

    add admin logout

---
+++ app/admin/orders/logout/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = NextResponse.redirect(new URL("/", process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"));
+
+  res.cookies.set("vsp_admin", "", {
+    path: "/",
+    maxAge: 0,
+  });
+
+  return res;
+}
